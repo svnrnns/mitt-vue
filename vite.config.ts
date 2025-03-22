0baccbaae4b0c0e87371a7863312277af6561578
svnrnns <haruueel@gmail.com>
2025-03-22 09:57:00 +0100

    fix: added vue plugins to vite-config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,27 @@
-import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
+  plugins: [
+    vue(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
       entry: 'src/index.ts',
       fileName: (format) => `mitt-vue.${format === 'es' ? 'js' : 'cjs'}`,
       formats: ['es', 'cjs'],
     },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
   },
-  plugins: [
-    dts({
-      insertTypesEntry: true,
-    }),
-  ],
 });
